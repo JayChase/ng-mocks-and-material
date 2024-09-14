34b7728c8f9bfe3b44670d3d25ec6eb856d39f2c
JayChase <jonathanchase@outlook.com>
2024-09-14 11:57:16 +0900

    show the problem

--- src/app/mac-guffin/mac-guffin.component.spec.ts
+++ src/app/mac-guffin/mac-guffin.component.spec.ts
@@ -1,23 +1,59 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import {
+  MatSnackBar,
+  MatSnackBarRef,
+  TextOnlySnackBar,
+} from '@angular/material/snack-bar';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MockBuilder, MockedComponentFixture, MockRender } from 'ng-mocks';
 import { MacGuffinComponent } from './mac-guffin.component';
 
 describe('MacGuffinComponent', () => {
-  let component: MacGuffinComponent;
-  let fixture: ComponentFixture<MacGuffinComponent>;
+  let fixture: MockedComponentFixture<MacGuffinComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MacGuffinComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(MacGuffinComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    return MockBuilder(MacGuffinComponent)
+      .beforeCompileComponents((testBed) => {
+        testBed.overrideComponent(MacGuffinComponent, {
+          set: {
+            changeDetection: ChangeDetectionStrategy.Default, // onPush doesn't work well with detectChanges()
+            providers: [], // clear out the component level providers so the mocks get used
+          },
+        });
+      })
+      .keep(CommonModule)
+      .keep(NoopAnimationsModule)
+      .keep(MatButtonModule);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(async () => {
+    fixture = MockRender(MacGuffinComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
+  });
+
+  describe('ok button', () => {
+    let okHarness: MatButtonHarness;
+    let matSnackBar!: MatSnackBar;
+    const mockMatSnackBarRef = {} as MatSnackBarRef<TextOnlySnackBar>;
+
+    beforeEach(async () => {
+      okHarness = await loader.getHarness(
+        MatButtonHarness.with({ selector: '.mac-guffin__ok' })
+      );
+
+      matSnackBar = fixture.componentRef.injector.get(MatSnackBar);
+      spyOn(matSnackBar, 'open').and.returnValue(mockMatSnackBarRef);
+    });
+
+    it('should show a snackbar', async () => {
+      await okHarness.click();
+      expect(matSnackBar.open).toHaveBeenCalled();
+    });
   });
 });
